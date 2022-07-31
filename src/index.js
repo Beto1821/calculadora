97cec7dec261fc6baee4bceddaacda115f402211
beto1821@uol.com.br <Katia@1006>
2022-07-30 22:42:32 -0300

    elementos main & fonts

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Calculator from './main/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+    <h1>Calculator</h1>
+    <Calculator />
+    </div>
+
   </React.StrictMode>
 );
 
